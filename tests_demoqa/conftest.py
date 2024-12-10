08b898e30c8c3d7536e118016645aa56c3509cce
Yaroslav Gusev <yarigusev@gmail.com>
2024-12-10 15:50:10 +0300

    mid-level-step-objects

--- tests_demoqa/conftest.py
+++ tests_demoqa/conftest.py
@@ -8,4 +8,4 @@ def open_browser():
     driver_options = webdriver.ChromeOptions()
     driver_options.page_load_strategy = 'eager'
     browser.config.driver_options = driver_options
-    browser.config.base_url = 'https://demoqa.com/automation-practice-form'
+    browser.config.base_url = 'https://demoqa.com'
