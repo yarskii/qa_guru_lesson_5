f0ad7b961f59fde95ee04ef6e2c7c60efb74a2b6
Yaroslav Gusev <yarigusev@gmail.com>
2024-12-17 17:47:33 +0300

    add parametrized

--- tests/conftest.py
+++ tests/conftest.py
@@ -7,30 +7,41 @@ import os
 
 from resources import attach
 
+DEFAULT_BROWSER_VERSION = "126.0"
+
+
+def pytest_addoption(parser):
+    parser.addoption(
+        '--browser_version',
+        default='100.0'
+    )
+
 
 @pytest.fixture(scope="session", autouse=True)
 def load_env():
     load_dotenv()
-    selenoid_login = os.getenv("SELENOID_LOGIN")
-    selenoid_pass = os.getenv("SELENOID_PASS")
-    selenoid_url = os.getenv("SELENOID_URL")
-
-    return selenoid_login, selenoid_pass, selenoid_url
 
 
 @pytest.fixture(scope='session')
-def open_browser(load_env):
-    selenoid_login, selenoid_pass, selenoid_url = load_env
+def open_browser(request):
+    browser_version = request.config.getoption('--browser_version')
+    browser_version = browser_version if browser_version != "" else DEFAULT_BROWSER_VERSION
+
     options = Options()
     selenoid_capabilities = {
         "browserName": "chrome",
-        "browserVersion": "126.0",
+        "browserVersion": browser_version,
         "selenoid:options": {
             "enableVNC": True,
             "enableVideo": True
         }
     }
     options.capabilities.update(selenoid_capabilities)
+
+    selenoid_login = os.getenv("SELENOID_LOGIN")
+    selenoid_pass = os.getenv("SELENOID_PASS")
+    selenoid_url = os.getenv("SELENOID_URL")
+
     driver = webdriver.Remote(
         command_executor=f"https://{selenoid_login}:{selenoid_pass}@{selenoid_url}/wd/hub",
         options=options
